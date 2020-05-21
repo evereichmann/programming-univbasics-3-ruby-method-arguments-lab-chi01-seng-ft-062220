0bab186eb01850f3d74148cbd3f3946bc6b625eb
Eve Reichmann <ereichmann4@aol.com>
2020-05-21 17:23:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,8 +6,8 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name="Eve", language="Ruby")
-  "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language_optional(name, language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction "Eve"
@@ -105,5 +105,5 @@ introduction_with_language_optional "name", "language"
 #end
 
 #def introduction_with_language_optional(name, language="Ruby")
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#  puts "Hi, my name is #{name} and I am learning to program in #{language}.""
 #end
